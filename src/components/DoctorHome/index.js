185106a0f4e33e9c758464facd8b9430ec38f1a6
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-24 18:48:55 +0300

    basic components

---
+++ src/components/DoctorHome/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const DoctorHome = () => {
+  return <div>DoctorHome</div>;
+};
+
+export default DoctorHome;
---
+++ src/components/PatientHome/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const PatientHome = () => {
+  return <div>PatientHome</div>;
+};
+
+export default PatientHome;
